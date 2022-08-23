b5f94f86ab6131d718cebf6eeaeb0a4ff2bc464c
Tim Phan <timothyphan123321@gmail.com>
2022-08-23 12:58:33 -0500

    Finished

--- Project Euler/Problem 5.py
+++ Project Euler/Problem 5.py
@@ -1,2 +1,7 @@
 # Smallest multiple of 20
+import math
 
+ans = 1
+for i in range(1, 21):
+    ans *= i // math.gcd(ans, i)
+print(ans)
--- practice.py
+++
@@ -1,9 +0,0 @@
-def compute():
-    ans = max(i * j
-              for i in range(100, 1000)
-              for j in range(100, 1000)
-              if str(i * j) == str(i * j)[::-1])
-    return str(ans)
-
-
-print(compute())
