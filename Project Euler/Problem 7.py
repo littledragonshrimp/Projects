5b127960866e0f7a56b291f65346c2770323b80a
Tim Phan <timothyphan123321@gmail.com>
2022-08-23 17:03:50 -0500

    init

---
+++ Project Euler/Problem 7.py
@@ -0,0 +1,30 @@
+# 10,001st prime #
+
+import math
+
+
+def compute():
+    count = 0
+    lastprime = None
+    n = 1
+    while count != 10001:
+        a = prime(n)
+        if a != -1:
+            count += 1
+            lastprime = n
+            n += 1
+        else:
+            continue
+    return str(lastprime)
+
+
+def prime(n):
+    for i in range(2, math.sqrt(n) + 1):
+        if n % i == 0:
+            return (-1)
+
+        else:
+            return (n)
+
+
+print(compute())
