acfbab9da1921383fb73c400d8f07a70aeab8523
Tim Phan <timothyphan123321@gmail.com>
2022-08-24 12:59:03 -0500

    changes

--- Project Euler/Problem 7.py
+++ Project Euler/Problem 7.py
@@ -19,12 +19,9 @@ def compute():
 
 
 def prime(n):
-<<<<<<< HEAD
     for i in range(2, math.isqrt(n) + 1):
-=======
-    uprange = math.isqrt(n + 1)
+        uprange = math.isqrt(n + 1)
     for i in range(2, uprange):
->>>>>>> b48540604e858b759b3c2236e4c87e7c891b57a0
         if n % i == 0:
             return (-1)
 
