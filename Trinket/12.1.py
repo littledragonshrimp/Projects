2c3ebc96c7126a3656e1afbe6d1318ca1b2b9da9
Tim Phan <timothyphan123321@gmail.com>
2022-09-09 07:24:19 -0500

    innit

--- Trinket/12.1.py
+++ Trinket/12.1.py
@@ -1,6 +1,9 @@
+# read web page
+
 import socket
 import re
 
+<<<<<<< Updated upstream
 inp = input('Enter URL: ')
 host = re.findall('.+//(.+)/.+', inp)
 
@@ -21,3 +24,24 @@ while True:
     print(data.decode(), end='')
 
 sock.close()
+=======
+inp = input('Input URL: ')
+try:
+    host = inp.split('/')
+    ghost = host[2]
+
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect((ghost, 80))
+    cmd = 'GET inp HTTP/1.0\r\n\r\n'.encode()
+    mysock.send(cmd)
+
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        print(data.decode(), end='')
+
+except TypeError:
+    print('Please enter valid URL.')
+mysock.close()
+>>>>>>> Stashed changes
--- Trinket/practice.py
+++ Trinket/practice.py
@@ -1,5 +1,11 @@
+<<<<<<< Updated upstream
 string = 'The quick brown fox jumps over the lazy dog'
 test = 'attach'
 encode = string.encode()
 print(test.__add__(' ' + encode.decode()))
 
+=======
+inp = 'http://data.pr4e.org/romeo.txt'
+host = inp.split('/')
+print(host[2])
+>>>>>>> Stashed changes
