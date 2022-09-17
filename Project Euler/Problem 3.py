0f8fbeff0f75344c61f2eb47b71d1f1907b50b20
Tim Phan <timothyphan123321@gmail.com>
2022-09-17 07:56:43 -0500

    init

--- Project Euler/Problem 3.py
+++ Project Euler/Problem 3.py
@@ -1,12 +1,18 @@
 # Find the largest prime factor of 600851475143
-
+# UNFINISHED
 num = 600851475143
 largeprime = None
+split = num // 2
 
 
 def compute():
-    for i in reversed(range(num / 2, num)):
-        
+    for i in reversed(range(split, num)):
+        for n in range(2, i // 2):
+            if i % n == 0:
+                largeprime = i
+            else:
+                continue
+    return largeprime
 
 
 print(compute())
